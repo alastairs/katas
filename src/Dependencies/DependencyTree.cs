b93ee5dca69f0a756b497fa1afd13b5b79902a19
Alastair Smith <github@alastairsmith.me.uk>
2025-01-02 10:33:55 +0000

    Add first dependency tree test
    Components added to the tree should be present within the tree.

---
+++ src/Dependencies/DependencyTree.cs
@@ -0,0 +1,14 @@
+﻿using Katas.Dependencies.Parsing;
+
+namespace Katas.Dependencies;
+
+public class DependencyTree
+{
+    private readonly Dictionary<string, SortedSet<string>> _tree = new();
+
+    public void Add(Component component) =>
+        _tree.Add(component.Name, new SortedSet<string>(component.Dependencies));
+
+    public bool Contains(string componentName) =>
+        _tree.ContainsKey(componentName);
+}
---
+++ test/Katas.Dependencies.Tests/DependencyTreeFacts.cs
@@ -0,0 +1,12 @@
+﻿namespace Katas.Dependencies.Tests;
+
+public class DependencyTreeFacts
+{
+    [Fact]
+    public void Components_are_added_to_the_tree()
+    {
+        var sut = new DependencyTree();
+        sut.Add(new Component("A", ["B", "C"]));
+        Assert.True(sut.Contains("A"));
+    }
+}
