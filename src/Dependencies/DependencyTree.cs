951f9194accc3922ed8395f4e32092023b4f980f
Alastair Smith <github@alastairsmith.me.uk>
2025-01-02 10:48:06 +0000

    Dependencies are added to the tree
    When a new component is added that is a dependency of other components,
    its own dependencies should be added to its dependents' dependencies.

--- src/Dependencies/DependencyTree.cs
+++ src/Dependencies/DependencyTree.cs
@@ -6,9 +6,23 @@ public class DependencyTree
 {
     private readonly Dictionary<string, SortedSet<string>> _tree = new();
 
-    public void Add(Component component) =>
+    public void Add(Component component)
+    {
         _tree.Add(component.Name, new SortedSet<string>(component.Dependencies));
+
+        // Update existing components' dependencies
+        foreach (var (_, dependencies) in _tree)
+        {
+            if (dependencies.Contains(component.Name))
+            {
+                dependencies.UnionWith(component.Dependencies);
+            }
+        }
+    }
 
     public bool Contains(string componentName) =>
         _tree.ContainsKey(componentName);
+
+    public IEnumerable<string> DependenciesFor(string componentName) =>
+        _tree[componentName];
 }
--- test/Katas.Dependencies.Tests/DependencyTreeFacts.cs
+++ test/Katas.Dependencies.Tests/DependencyTreeFacts.cs
@@ -9,4 +9,15 @@ public class DependencyTreeFacts
         sut.Add(new Component("A", ["B", "C"]));
         Assert.True(sut.Contains("A"));
     }
+
+    [Fact]
+    public void Dependencies_are_added_to_the_tree()
+    {
+        var sut = new DependencyTree();
+
+        sut.Add(new Component("A", ["B", "C"]));
+        sut.Add(new Component("B", ["C", "E"]));
+
+        Assert.Equal(["B", "C", "E"], sut.DependenciesFor("A"));
+    }
 }
