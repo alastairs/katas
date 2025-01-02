e43086c6079956e07bdf7d441bb20d8bd9244413
Alastair Smith <github@alastairsmith.me.uk>
2025-01-02 12:51:49 +0000

    Scaffold inital CLI wrapper
    Add Spectre.Console, and a strongly-typed PrintCommand to call the Print
    function.

---
+++ src/Dependencies/Cli/PrintCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using Katas.Dependencies.DependenciesTree;
+using Spectre.Console.Cli;
+
+namespace Katas.Dependencies.Cli;
+
+[Description("Prints the specified dependencies tree")]
+public class PrintCommand : Command<PrintCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [CommandOption("-f|--file <FILE>")]
+        [Description("The dependency file to read from. Use [grey]-[/] to read from standard input.")]
+        public required FlagValue<string> FilePath { get; set; }
+
+        [CommandArgument(0, "<LIST>")]
+        public required string Input { get; set; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var input = settings.Input;
+
+        if (settings.FilePath.IsSet)
+        {
+            var stream = settings.FilePath switch
+            {
+                { IsSet: true } and { Value: "-" } => Console.OpenStandardInput(),
+                _ => File.OpenRead(settings.FilePath.Value)
+            };
+
+            using var reader = new StreamReader(stream);
+            input = reader.ReadToEnd();
+        }
+
+        Console.WriteLine(DependencyTree.Print(input));
+        return 0;
+    }
+}
--- src/Dependencies/Program.cs
+++ src/Dependencies/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Katas.Dependencies.Cli;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.SetApplicationName("deps");
+    config.AddCommand<PrintCommand>("print");
+});
+
+return app.Run(args);
